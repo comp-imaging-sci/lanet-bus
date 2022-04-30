dc9259c19a3b256777fed9b14f34ef35aa4a2766
CasiaFan <fanzongshaoxing@gmail.com>
2022-04-30 12:38:41 -0500

    train with pre-trained saliency

--- run_train.sh
+++ run_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # matlab -nodisplay -nosplash -nodesktop -r "run('/home/xiaohui8/Desktop/tube_samples_dataset/GoogLeNet/googlenet_pretrain.m');exit;"|tail -n +11
 model_name="resnet50_cbam_mask"
-image_size=512
+image_size=256
 map_size=$(expr $image_size / 32)
-datatype="MAYO"
-exp="exp12"
+datatype="BUSI"
+exp="exp9"
 num_classes=2
 use_mask=True
 channel_att=True
@@ -14,12 +14,14 @@ if [ ! -d $save_path ]; then
     mkdir $save_path
 fi 
 
-
-saliency_weight=""
-# backbone_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp6-resnet50-mask=False-channel_att=False-size=512-cls=2/best_model.pt"
-#backbone_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp5-resnet50-mask=False-channel_att=False-size=256-cls=2/best_model.pt"
+backbone_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp5-resnet50-mask=False-channel_att=False-size=256-cls=2/best_model.pt"
+saliency_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp17-resnet50_cbam_mask-mask=True-channel_att=True-size=256-cls=2/best_model.pt"
+#backbone_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp6-resnet50-mask=False-channel_att=False-size=512-cls=2/best_model.pt"
+#saliency_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp18-resnet50_cbam_mask-mask=True-channel_att=True-size=512-cls=2/best_model.pt"
 #backbone_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp7-resnet18-mask=False-channel_att=False-size=256-cls=2/best_model.pt"
+# saliency_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp19-resnet18_cbam_mask-mask=True-channel_att=True-size=256-cls=2/best_model.pt"
 #backbone_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp8-resnet18-mask=False-channel_att=False-size=512-cls=2/best_model.pt"
+# saliency_weight="/shared/anastasio5/COVID19/ultrasound_breast_cancer/All_train/exp20-resnet18_cbam_mask-mask=True-channel_att=True-size=512-cls=2/best_model.pt"
 
 python train.py --model_name=$model_name \
                 --image_size=$image_size \
@@ -41,7 +43,7 @@ python train.py --model_name=$model_name \
                 --attention_kernel_size=3 \
                 --attention_num_conv=3 \
                 --backbone_weights="$backbone_weight"\
-                --saliency_weights=$saliency_weight\
+                --saliency_weights="$saliency_weight"\
                 --mask_weight=1
                 #--mask_annotate_file="data/mayo_patient_info.csv" \
                 #--pretrained_weights="/shared/anastasio5/COVID19/ultrasound_breast_cancer/MAYO_resnet50_mask_448/best_model.pt" \
@@ -59,13 +61,13 @@ python train.py --model_name=$model_name \
 # resnet18 mask=False channel_att=True class=2 All
 
 # exp 9-10, size = {256, 512}
-# resnet50-cbam-mask mask=True channel_att=True class=2 BUSI
+# resnet50-cbam-mask mask=True channel_att=True class=2 BUSI (load pre-trained saliency)
 
 # exp 11-12, size = {256, 512}
-# resnet50-cbam-mask mask=True channel_att=True class=2 MAYO
+# resnet50-cbam-mask mask=True channel_att=True class=2 MAYO (load pre-trained saliency)
 
 # exp 13-14, size = {256, 512}
-# resnet18-cbam-mask mask=True channel_att=True class=2 BUSI
+# resnet18-cbam-mask mask=True channel_att=True class=2 BUSI (load pre-trained saliency)
 
 # exp 15-16, size = {256, 512}
-# resnet18-cbam-mask mask=True channel_att=True class=2 MAYO
+# resnet18-cbam-mask mask=True channel_att=True class=2 MAYO (load pre-trained saliency)
